42a1a7de8229809af5c7c889a2787f024fc1e302
Jesse Michael Han <hyrodi@gmail.com>
2019-02-25 12:08:31 -0500

    Check if goal has remaining lambdas before attempting to rewrite
    move examples out of tactic namespace to new section

--- src/smt/preprocess.lean
+++ src/smt/preprocess.lean
@@ -1,12 +1,16 @@
 import tactic.finish tactic.tidy
 
+section hewwo
+
+end hewwo
+
+
 open expr
 namespace tactic
-
 section intro_ext
 
 meta def intro_ext_tactics : list (tactic string) :=
-[ tactic.reflexivity                                 >> pure "refl",
+[ -- tactic.reflexivity                                 >> pure "refl",
   -- `[exact dec_trivial]                        >> pure "exact dec_trivial",
   -- propositional_goal >> assumption            >> pure "assumption",
   tidy.ext1_wrapper,
@@ -23,12 +27,48 @@ meta def intro_ext_tactics : list (tactic string) :=
 
 meta def intro_ext_cfg : tidy.cfg := {tactics := intro_ext_tactics}
 
--- example : (λ x : ℕ, x + 1) = λ x, 0 + x + 0 + 1  :=
--- begin
---  tidy? intro_ext_cfg,
--- end
+
 
 end intro_ext
+
+section is_first_order
+
+/- Checks if an an expr contains a quantification over types -/
+meta def contains_type_variables : expr → bool
+  | (lam a b c d) := is_sort c ∨ contains_type_variables d
+  | (var n) := ff
+  | (sort l) := ff
+  | (const a b) := ff
+  | (mvar a b c) := contains_type_variables c
+  | (local_const a b c d) := contains_type_variables d
+  | (app a b) := contains_type_variables a ∨ contains_type_variables b
+  | (pi a b c d) := is_sort c ∨ contains_type_variables d
+  | (elet a b c d) := is_sort c ∨ contains_type_variables b ∨ contains_type_variables d
+  | (macro a b) := (b.map contains_type_variables).foldr (λ x y, x ∨ y) ff
+
+meta def contains_lambda : expr → bool
+  | (lam a b c d) := tt
+  | (var n) := ff
+  | (sort l) := ff
+  | (const a b) := ff
+  | (mvar a b c) := contains_lambda c
+  | (local_const a b c d) := contains_lambda d
+  | (app a b) := contains_lambda a ∨ contains_lambda b
+  | (pi a b c d) := contains_lambda c ∨ contains_lambda d
+  | (elet a b c d) := contains_lambda c ∨ contains_lambda b ∨ contains_lambda d
+  | (macro a b) := (b.map contains_lambda).foldr (λ x y, x ∨ y) ff
+
+meta def is_lambda_free : tactic bool := target >>= (λ e, (return $ ¬ contains_lambda e))
+
+meta def is_first_order : expr → bool := λ e, ¬ (contains_lambda e ∨ contains_type_variables e)
+
+meta def is_first_order_goal : tactic bool := target >>= (λ e, (return $ is_first_order e))
+
+meta def is_first_order_goal_trace : tactic unit := is_first_order_goal >>= tactic.trace
+
+
+
+end is_first_order
 
 section lambda_lifting
 
@@ -69,10 +109,18 @@ do -- tp <- infer_type v,
      -- trace h,
      -- tactic.dsimp_target s []
      -- hyps.mmap (λ h, simp_hyp s [] h),
-     trace "simp_hyp succeeded",
-     tactic.try $ interactive.simp_core_aux {} (tactic.failed) s [] [h] tt,
-     tgt <- (target >>= pp),
-     trace ("target after simp call is now... \n ⊢ " ++ (to_string (tgt)))
+     target >>= pp >>=
+       (λ x, (trace $ string.append "target before simp call is \n ⊢ " (to_string (x)))),
+     tactic.try $ interactive.simp_core_aux {eta := ff, beta := ff} (tactic.failed) s [] [h] tt,
+     target >>= pp >>=
+       (λ x, (trace $ string.append "target after simp call is \n ⊢ " (to_string (x))))
+
+#print notation >>
+#print notation <|>
+
+meta def skip_if_lambda_free : tactic unit :=
+is_lambda_free >>=
+  λ b, if b then skip else tactic.fail "target still contains λ"
 
 meta def lambda_set : expr → tactic unit
   | (var n) := return ()
@@ -81,14 +129,19 @@ meta def lambda_set : expr → tactic unit
   | (mvar a b c) := lambda_set c
   | (local_const a b c d) := lambda_set d 
   | (app a b) := lambda_set a >> lambda_set b
-  | (lam a b c d) := tidy intro_ext_cfg >> -- trace_goal_raw >> trace "tidy was called" >>
-  (target >>= lambda_set) <|> tidy intro_ext_cfg <|>
-                    fail_if_no_goals >>
-                    -- trace_goal_raw >>
+  /- Upon encountering a lambda, attempts to call tidy with only intro and ext to remove it
+     from the goal, and then calls 'lambda_set' on the target again.
+     If tidy fails, then if the goal is lambda-free, the tactic's job is done.
+     If the goal is not lambda free, then lambda_set_core is called to perform lambda-lifting. -/
+  | (lam a b c d) := (tidy intro_ext_cfg >>
+  ((skip_if_lambda_free) <|> target >>= lambda_set)) <|>
+                    (skip_if_lambda_free <|>
                     do n <- (tactic.get_unused_name "HEWWO"),
-                        n' <- (tactic.get_unused_name "HERRO"),
+                       n' <- (tactic.get_unused_name "HERRO"),
+                       l <- pp (lam a b c d),
                         tactic.trace "lambda_set_core is being called again",
-                        tactic.lambda_set_core n n' (lam a b c d)
+                        trace $ "it will be rewriting" ++ (to_string l),
+                        tactic.lambda_set_core n n' (lam a b c d))
   | (pi a b c d) := -- tidy intro_ext_cfg >> trace "tidy was called'" >>  target >>= lambda_set <|> fail_if_no_goals
                     -- >>  
                     lambda_set c >> lambda_set d
@@ -106,71 +159,13 @@ meta def trace_intro : tactic unit :=
 
 meta def lambda_set_tactic : tactic unit := target >>= lambda_set
 
--- tidy intro_ext_cfg >> (target >>= lambda_set)
-example : (λ x : ℕ, x + 1) = λ x, 0 + x + 0 + 1  :=
-begin
-  lambda_set_tactic, simp
-
-/- tactic state is now
-  HEWWO : ℕ → ℕ := λ (x : ℕ), x + 1,
-  HERRO : ∀ (x : ℕ), HEWWO x = x + 1,
-  HERRO._rw : (λ (x : ℕ), x + 1) = HEWWO,
-  HEWWO_1 : ℕ → ℕ := λ (x : ℕ), 0 + x + 0 + 1,
-  HERRO_1 : ∀ (x : ℕ), HEWWO_1 x = 0 + x + 0 + 1,
-  HERRO_1._rw : (λ (x : ℕ), 0 + x + 0 + 1) = HEWWO_1
-  ⊢ HEWWO = HEWWO_1
--/
-end
-
 /-
 Note: the call to the simplifier annoyingly simplifies other parts of the target along with the intended rewrite. Possible fix: using rw instead of simp? Or use expr.replace and then change?
-
-Also todo: hook this up to a fragment of tidy or chain, to keep trying to apply extensionality lemmas. The example above is vacuous because we can get rid of the lambdas with funext. After `chain` is finished, use `expr.is_sort` to check if there are any quantifications over sorts
 
 In case we have assumptions with universal quantifications over types, we should also populate a list with all types which occur in the initial goal, plus types relevant to the target logic, with which to instantiate such quantifications
 -/
 
 end lambda_lifting
-
-section is_first_order
-
-/- Checks if an an expr contains a quantification over types -/
-meta def contains_type_variables : expr → bool
-  | (lam a b c d) := is_sort c ∨ contains_type_variables d
-  | (var n) := ff
-  | (sort l) := ff
-  | (const a b) := ff
-  | (mvar a b c) := contains_type_variables c
-  | (local_const a b c d) := contains_type_variables d
-  | (app a b) := contains_type_variables a ∨ contains_type_variables b
-  | (pi a b c d) := is_sort c ∨ contains_type_variables d
-  | (elet a b c d) := is_sort c ∨ contains_type_variables b ∨ contains_type_variables d
-  | (macro a b) := (b.map contains_type_variables).foldr (λ x y, x ∨ y) ff
-
-meta def contains_lambda : expr → bool
-  | (lam a b c d) := tt
-  | (var n) := ff
-  | (sort l) := ff
-  | (const a b) := ff
-  | (mvar a b c) := contains_lambda c
-  | (local_const a b c d) := contains_lambda d
-  | (app a b) := contains_lambda a ∨ contains_lambda b
-  | (pi a b c d) := contains_lambda c ∨ contains_lambda d
-  | (elet a b c d) := contains_lambda c ∨ contains_lambda b ∨ contains_lambda d
-  | (macro a b) := (b.map contains_lambda).foldr (λ x y, x ∨ y) ff
-
-meta def is_first_order : expr → bool := λ e, ¬ (contains_lambda e ∨ contains_type_variables e)
-
-meta def is_first_order_goal : tactic bool := target >>= (λ e, (return $ is_first_order e))
-
-meta def is_first_order_goal_trace : tactic unit := is_first_order_goal >>= tactic.trace
-
--- example : (λ x : ℕ, x + 1) = λ x, 0 + x + 0 + 1  :=
--- begin
---  tidy? intro_ext_cfg, is_first_order_goal_trace
--- end
-
-end is_first_order
 
 section type_scraper
 
@@ -178,6 +173,9 @@ section type_scraper
 /- We use this to avoid duplicate types when instantiating -/
 def cons_new {α : Type*} [decidable_eq α] (a : α) (xs : list α) : list α :=
   if a ∈ xs then xs else a :: xs
+
+--note from Simon: use this instead of cons_new
+-- #check native.rb_set.insert
 
 local infix `::'`:50 := cons_new
 
@@ -197,10 +195,6 @@ meta def types_in_expr  : expr →  tactic (list expr)
 meta def types_in_goal : tactic unit :=
   target >>= types_in_expr >>= tactic.trace
 
--- example : ∀ p : Prop, ∀ q : Prop, p ∧ q ↔ q ∧ p  :=
--- begin
---   types_in_goal,
--- end
 end type_scraper
 
 section preprocessor -- TODO
@@ -212,19 +206,49 @@ do tgt                <- target,
     -- trace instantiation_list
    >> lambda_set_tactic,
    do first_order_goal_flag <- is_first_order_goal,
-      if first_order_goal_flag then return () else tactic.trace "Preprocessing failed, target is not first-order"
-
-example : ∀ f : ℕ → ℕ, ∀ g : ℕ → ℕ → ℕ, (g $ f 0 ) 0 = (λ x y, g x y) (f 0) 0 ∧ ∀ p q : ℕ → Prop, ∀ r : Prop, (p 0) ↔ r ↔ ∀ r : Prop, (q 0) ↔ r
-:=
-begin
-  preprocess, tidy, sorry
-end
-
-example : (λ x : ℕ, x + 1) = λ x, 0 + x + 0 + 1  :=
-begin preprocess, simp end
+      if first_order_goal_flag then return () else tactic.fail "Preprocessing failed, target is not first-order"
 
 -- TODO perform instantiations using congr_fun after applying extensionality lemmas
 
 end preprocessor
-  
 end tactic
+
+section test
+open tactic
+
+-- example : (λ x : ℕ, x + 1) = λ x, 0 + x + 0 + 1  :=
+-- begin
+--  tidy? intro_ext_cfg,
+-- end
+
+-- example : (λ x : ℕ, x + 1) = λ x, 0 + x + 0 + 1  :=
+-- begin
+--  tidy? intro_ext_cfg, is_first_order_goal_trace
+-- end
+
+example : (λ x : ℕ, x + 1) = λ x, 0 + x + 0 + 1  :=
+begin
+  lambda_set_tactic, finish
+end
+
+-- example : (λ x : ℕ, x) = (λ x, id x) :=
+-- begin
+--  ext
+-- end
+
+
+-- example : ∀ p : Prop, ∀ q : Prop, p ∧ q ↔ q ∧ p  :=
+-- begin
+--   types_in_goal,
+-- end
+
+
+example : ∀ f : ℕ → ℕ, ∀ g : ℕ → ℕ → ℕ, (g $ f 0 ) 0 = (λ x y, g x y) (f 0) 0 -- ∧ ∀ p q : ℕ → Prop, ∀ r : Prop, (p 0) ↔ r ↔ ∀ r : Prop, (q 0) ↔ r
+:=
+begin
+ preprocess,
+end
+
+example : (λ x : ℕ, x + 1) = λ x, 0 + x + 0 + 1  :=
+begin tidy? intro_ext_cfg, end
+end test
